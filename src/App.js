ddbda284e1656d89d702f876ff887f5c23b3a84e
saloni suryavanshi <salonisuryavanshi123>
2023-04-20 08:27:15 +0530

    plusmib

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useReducer } from 'react';
 
+let oldState ={
+  value:0
+};
+
+let reducerFunction = (oldState,action)=>{
+  console.log('oldState---->',oldState);
+  console.log('action--->',action.type)
+  switch(action.type){
+    case 'ADDITION':
+      return {...oldState, value:oldState.value+action.payload}
+      break;
+    case 'SUBTRACTION':
+      return {...oldState, value:oldState.value-action.payload}
+      break;
+      
+    default:
+      return oldState; 
+  }
+
+}
 function App() {
+  const [newState,dispatch] = useReducer(reducerFunction,oldState)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>{newState.value}</h1>
+      <button onClick={()=>{ dispatch({type:"ADDITION",payload:1}) }}>+</button>
+      <button onClick={()=>{ dispatch({type:"SUBTRACTION",payload:1}) }}>-</button>
     </div>
   );
 }
